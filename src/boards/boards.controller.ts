067d1caa4f831c687277215ebeb2c1a3cf6c427e
Kim Kiheon <rtyu456456@gmail.com>
2024-10-02 12:05:50 +0900

    20241002 am

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -12,4 +13,29 @@ export class BoardsController {
         return this.boardsService.getAllBoards();
     }
 
+    @Post()
+    @UsePipes(ValidationPipe)
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardsService.createBoard(createBoardDto)
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string){
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard;
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus
+    ) {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
 }
